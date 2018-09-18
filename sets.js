9737193900d8d52d4561fa6061d4f2c6781618d4
christophermayfield <christopher.j.mayfield@gmail.com>
2018-09-18 11:34:08 -0700

    Inital upload for sets.js

---
+++ sets.js
@@ -0,0 +1,16 @@
+/* Sets */
+
+function mySet(){
+    //the collection will hold the set
+    var collection = [];
+    
+    //this method will check for the presence of values and return true or false.
+    this.has = function(element){
+        return (collection.indexOf(element)!== -1);
+    }
+    
+    //this method will return the valuesDD
+    this.values = function(){
+        return collection;
+    }
+}
