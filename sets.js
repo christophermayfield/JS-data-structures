44856918a379e55f54b6db8ed29d0d0642014f0d
christophermayfield <christopher.j.mayfield@gmail.com>
2018-09-18 12:19:00 -0700

    update code standards

--- sets.js
+++ sets.js
@@ -8,22 +8,33 @@ function mySet(){
     //this method will check for the presence of values and return true or false.
     this.has = function(element){
         return (collection.indexOf(element)!== -1);
-    }
+    };
     
     //this method will return the values in the set
     this.values = function(){
         return collection;
-    }
+    };
 
-//this method will add an elemetn to the set 
+//this method will add an element to the set 
     this.add = function(element){
         if(!this.has(element)){
             collection.push(element);
             return true;
         }
         return false;
+    };
+//this method will remove an element from the list 
+    this.remove = function(element){
+        if(this.has(element)){
+            index = collection.indexOf(element)
+            collection.splice(index,1);
+            return true;
+        }
+        return false;
     }
 
+
+//endset
 };
 
 
