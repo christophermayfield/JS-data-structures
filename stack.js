f77fb6cd5bb5834be9389ee37bf350de90e842d4
christophermayfield <christopher.j.mayfield@gmail.com>
2018-09-17 23:30:22 -0700

    add stack data structure

---
+++ stack.js
@@ -0,0 +1,44 @@
+//creates a stack
+var Stack = function(){
+    this.count = 0;
+    this.storage = {};
+
+
+//Adds a value on the end of the stack
+this.push = function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+    }
+
+//Removes and returns the value at the end of the stack
+this.pop = function(){
+    if(this.count == 0) {
+        return undefined;
+    }
+
+    this.count--;
+    var result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+    
+
+}
+    
+   //returns the size of the stack     
+    this.size = function(){
+        return this.count;
+    }
+
+//returns the value of the stack 
+    this.peek = function() {
+        return this.storage[this.count-1];
+    }
+
+}
+
+var myStack = new Stack();
+myStack.push(1);
+myStack.push(25);
+myStack.push(3);
+myStack.push(8);
+myStack.pop();
