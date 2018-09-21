1341644031aa973bb6fd5a1d5d3df2b9c687d3fe
christophermayfield <christopher.j.mayfield@gmail.com>
2018-09-21 01:11:38 -0700

    add BST class

---
+++ bst.js
@@ -0,0 +1,17 @@
+/* Binary Search Tree */ 
+
+class Node {
+    constructor(data, left = null, right = null) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+    
+}
+
+class BST {
+    constructor(){
+        this.root = null;
+        
+    }
+}
