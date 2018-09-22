9d3d4c6b6cc3cd137cb1e07ec9a27c707e5769e8
christophermayfield <christopher.j.mayfield@gmail.com>
2018-09-22 12:16:12 -0700

    add hashtable.js

--- bst.js
+++ bst.js
@@ -134,6 +134,32 @@ remove(data) {
    this.root = removeNode(this.root, data); 
 } 
 
+isBalanced(){
+    return(this.findMinHeight() >= this.findMaxHeight() -1);
+}
+
+findMinHeight(node = this.root) {
+    if(node == null) {
+        return -1;
+    };
+    let left = this.findMinHeight(node.left);
+    let right = this.findMinHeight(node.right);
+    if(left < right) {
+        return left + 1;
+    } else {
+        return right + 1;
+    };
+}
+
+findMaxHeight(node = this.root) {
+    if(node == null) {
+        return -1;
+    };
+    
+    let left = this.findMaxHeight(node.left);
+    let right = this.findMaxHeight(node.right);
+}
+
 
 const bst = new BST();
 bst.add(4);
