1b89b9666a5f72c6af20fb8ed49427afc1f5f3d9
christophermayfield <christopher.j.mayfield@gmail.com>
2018-09-24 15:15:53 -0700

    add remove function

--- hashtable.js
+++ hashtable.js
@@ -1,5 +1,6 @@
 /* Hash Tables */ 
 
+//simple hash function
 var hash = (string, max) => {
     var hash = 0;
     for(var i = 0; i < string.length; i++) {
@@ -14,16 +15,56 @@ let HashTable = function(){
     
     this.print = function() {
         console.log(storage);
-    }
+    };
     
     this.add = function(key, value) {
-        var index = hash(key, storageLimit) 
-        if(storage[index] === undefined) {
-            storage[index] = [
-                [key, value]
-            ];
-        } else {
-            var inserted = false;
+    var index = hash(key, storageLimit);
+    if (storage[index] === undefined) {
+      storage[index] = [
+        [key, value]
+      ];
+    } else {
+      var inserted = false;
+      for (var i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          storage[index][i][1] = value;
+          inserted = true;
         }
+      }
+      if (inserted === false) {
+        storage[index].push([key, value]);
+      }
     }
+  };
+    
+    this.remove = function(key) {
+    var index = hash(key, storageLimit);
+    if (storage[index].length === 1 && storage[index][0][0] === key) {
+      delete storage[index];
+    } else {
+      for (var i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          delete storage[index][i];
+        }
+      }
+    }
+  };
+    
+  
+    this.lookup = function(key) {
+    var index = hash(key, storageLimit);
+    if (storage[index] === undefined) {
+      return undefined;
+    } else {
+      for (var i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          return storage[index][i][1];
+        }
+      }
+    }
+  };
+
+};
+    
+    
 }
