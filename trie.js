734bc25855d1be8626ce076c53d306f84ded8f43
christophermayfield <christopher.j.mayfield@gmail.com>
2018-09-24 23:09:11 -0700

    update add function

--- trie.js
+++ trie.js
@@ -18,8 +18,14 @@ let Trie = function() {
     
     this.add = function(input, node = this.root) {
         if(input.length == 0) {
-            
-        }
-    }
+            node.setEnd();
+            return;
+        } else if (!node.keys.has(input[0])){
+            node.keys.set(input.substr(1), node.keys.get(input[0]));
+            return this.add(input.substr(1), node.keys.get(input[0]));
+        } else {
+            return this.add(input.substr(1), node.keys.get(input[0]));
+        };
+    };
     
 } //end Trie
