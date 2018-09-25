cfa76355666887d8774b14f5641471ec52ffff00
christophermayfield <christopher.j.mayfield@gmail.com>
2018-09-24 18:28:29 -0700

    add initial functionality to our linked list

---
+++ linkedlist.js
@@ -0,0 +1,13 @@
+/* Linked List */ 
+
+fucntion LinkedList() {
+var length = 0;
+var head = null;
+    
+var Node = function(element) {
+    this.element = element; //the thing that the list is going to have
+    this.next = null; //the link to the next node. 
+}
+    
+    
+};
