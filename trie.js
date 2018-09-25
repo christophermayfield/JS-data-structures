63f5d9ca676b289fbab3e0d633221bbef5c58032
christophermayfield <christopher.j.mayfield@gmail.com>
2018-09-24 23:02:38 -0700

    setting up the Trie

--- trie.js
+++ trie.js
@@ -1,1 +1,13 @@
 /* Trie Data Structure */ 
+/*Notes: 
+A trie is a special type of tree. A trie stores data in steps each step is a node in the trie, this is often used to store words */ 
+let Node = function() {
+    this.keys = new Map(); //ES6 structure, its like an object wtih key/value pairs
+    this.end = false;
+    this.setEnd = function() {
+        this.end = true;
+    };
+    this.isEnd = function() {
+        return this.end;
+    }
+}
