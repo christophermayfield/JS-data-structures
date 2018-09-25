ccc85f9ab97ad777a5cff97c99324caead57f288
christophermayfield <christopher.j.mayfield@gmail.com>
2018-09-24 18:43:13 -0700

    add remove function

--- linkedlist.js
+++ linkedlist.js
@@ -1,24 +1,38 @@
 /* Linked List */ 
 
 fucntion LinkedList() {
-var length = 0;
-var head = null;
+    var length = 0;
+    var head = null;
+
+    var Node = function(element) {
+        this.element = element; //the thing that the list is going to have
+        this.next = null; //the link to the next node. 
+    }
+    this.size = function() { //returns our length. 
+        return length;
+    }
+
+    this.head = function() { //returns the head of the linked list.
+        return head;
+    }
+
+    this.add = function() {
+        var node = new Node();
+        if(head === null) {
+            head = node;
+        } else {
+            var currentNode = head;
+
+            while(currentNode.next) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = node;
+        }
+        length++;
+    };
     
-var Node = function(element) {
-    this.element = element; //the thing that the list is going to have
-    this.next = null; //the link to the next node. 
-}
-this.size = function() { //returns our length. 
-    return length;
-}
-
-this.head = function() { //returns the head of the linked list.
-    return head;
-}
-
-this.add = function() {
-    var node = new Node();
-}
-
+    this.remove = function(element) {
+        
+    }
     
 };
