fe3d973c86b5734444b6bcfeaefaed1b9784d7a5
christophermayfield <christopher.j.mayfield@gmail.com>
2018-09-24 22:08:50 -0700

    add indexAt() function

--- linkedlist.js
+++ linkedlist.js
@@ -63,4 +63,17 @@ fucntion LinkedList() {
         return -1;
     }; 
     
-};
+    this.elementAt = function(index) {
+        var currentNode = head;
+        var count = 0;
+        
+        while(count < index) {
+            count++;
+            currentNode = currentNode.next;
+        }
+        return currentNode.element;
+    };
+
+
+
+}; //end LinkedList function
