20ff447ba84eaff3d407a7e7c5ca977f382d8b05
christophermayfield <christopher.j.mayfield@gmail.com>
2022-06-15 10:22:41 -0700

    upddate merge sort

--- mergesort.js
+++ mergesort.js
@@ -1,4 +1,4 @@
-//merge sort 
+//merge sort implemented in recursion.
 
 function mergeSort(unsortedArray) {
 
@@ -10,5 +10,13 @@ function mergeSort(unsortedArray) {
     
     //figuring out the middle by dividing the array in half
     const middle = Math.floor(unsortedArray.length / 2);
+    
+    // This is where we will be dividing the array into left and right
+    const left = unsortedArray.slice(0, middle);
+    const right = unsortedArray.slice(middle);
 
+  // Using recursion to combine the left and right
+    return merge(
+    merge(left), merge(right)
+  );
 }
