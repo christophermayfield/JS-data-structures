eccaf23ec5bf393cfab358e6ea3207614b800100
christophermayfield <christopher.j.mayfield@gmail.com>
2022-06-15 17:09:30 -0700

    update merge sort

--- mergesort.js
+++ mergesort.js
@@ -1,47 +1,109 @@
-//merge sort implemented in recursion.
+//merging two arrays appropriately.
+function merge(arr1, arr2) {
+  //make a new array and have two value pointers
+  let res = [],
+    i = 0,
+    j = 0;
 
-function mergeSort(unsortedArray) {
-
-
-//recursion base case:
-    if (unsortedArray <= 1) {
-        return unsortedArray;
-    }
-    
-    //figuring out the middle by dividing the array in half
-    const middle = Math.floor(unsortedArray.length / 2);
-    
-    // This is where we will be dividing the array into left and right
-    const left = unsortedArray.slice(0, middle);
-    const right = unsortedArray.slice(middle);
-
-  // Using recursion to combine the left and right
-    return merge(
-    merge(left), merge(right)
-  );
-}
-
-
-// Merge the two arrays: left and right
-function merge (left, right) {
-  let resultArray = [], leftIndex = 0, rightIndex = 0;
-
-  // concatenate values into the resultArray in order
-  while (leftIndex < left.length && rightIndex < right.length) {
-    if (left[leftIndex] < right[rightIndex]) {
-      resultArray.push(left[leftIndex]);
-      leftIndex++; // move left array cursor
-    } else {
-      resultArray.push(right[rightIndex]);
-      rightIndex++; // move right array cursor
+  //sorting the first array.
+  if (arr1.length > 1) {
+    let min = 0;
+    for (let i = 0; i < arr1.length; i++) {
+      if (i !== min) {
+        if (arr1[i] < arr1[min]) {
+          //also swap the elements
+          [arr1[i], arr1[min]] = [arr1[min], arr1[i]];
+          //change the minimum
+          min = i;
+        }
+      }
     }
   }
 
-  // We need to concat here because there will be one element remaining
-  // from either left side OR the right side
-  return resultArray
-          .concat(left.slice(leftIndex))
-          .concat(right.slice(rightIndex));
+  //sorting the second array.
+  if (arr2.length > 1) {
+    let min = 0;
+    for (let i = 0; i < arr2.length; i++) {
+      if (i !== min) {
+        if (arr2[i] < arr2[min]) {
+          //also swap the elements
+          [arr2[i], arr2[min]] = [arr2[min], arr2[i]];
+          //change the minimum
+          min = i;
+        }
+      }
+    }
+  }
+
+  //Value comparison.
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      res.push(arr1[i]);
+      i++;
+    } else {
+      res.push(arr2[j]);
+      j++;
+    }
+  }
+
+  //pushing the rest of arr1.
+  while (i < arr1.length) {
+    res.push(arr1[i]);
+    i++;
+  }
+
+  //pushing the rest of arr2.
+  while (j < arr2.length) {
+    res.push(arr2[j]);
+    j++;
+  }
+
+  return res;
 }
 
+//merge sort
+function mergeSort(arr) {
+  //Best case
+  if (arr.length <= 1) return arr;
 
+  //splitting into halves
+  let mid = Math.ceil(arr.length / 2);
+
+  let arr1 = arr.slice(0, mid);
+
+  let arr2 = arr.slice(mid);
+
+  let arr1_subarrays = [],
+    sorted_arr1_subarrays = [];
+
+  let arr2_subarrays = [],
+    sorted_arr2_subarrays = [];
+
+  //loop through array 1 making subarrays of two elements
+  for (let i = 0; i < arr1.length; i += 2) {
+    arr1_subarrays.push(arr1.slice(i, i + 2));
+  }
+
+  //loop through array 2 making subarrays of two elements.
+  for (let i = 0; i < arr2.length; i += 2) {
+    arr2_subarrays.push(arr2.slice(i, i + 2));
+  }
+
+  // sorting each subarray of arr1.
+  for (let i = 0; i < arr1_subarrays.length; i += 2) {
+    let result = merge(arr1_subarrays[i], arr1_subarrays[i + 1]);
+    result.forEach((value) => sorted_arr1_subarrays.push(value));
+  }
+
+  // sorting each subarray of arr2.
+  for (let i = 0; i < arr2_subarrays.length; i += 2) {
+    let result = merge(arr2_subarrays[i], arr2_subarrays[i + 1]);
+    result.forEach((value) => sorted_arr2_subarrays.push(value));
+  }
+
+  let result = merge(sorted_arr1_subarrays, sorted_arr2_subarrays);
+
+  return result;
+}
+
+console.log(mergeSort([70, 50, 30, 10, 20, 40, 60]));
