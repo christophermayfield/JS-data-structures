8e906115dd64b9895a3e46cf617cbf5d7eab00e8
christophermayfield <christopher.j.mayfield@gmail.com>
2022-06-19 23:44:21 -0700

    add swap function

--- heapsort.js
+++ heapsort.js
@@ -18,4 +18,12 @@ class MaxHeap {
     rightChildIndex(index){
         return (2*index + 2);
     }
+
+    swap(a,b) {
+        let temp = this.heap[a];
+        this.heap[a] = this.heap[b];
+        this.heap[b] = temp;
+    }
+
 }
+
