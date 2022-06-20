22650e5e2b6eeeedca9d12d54d8bd63d8ce5f97b
christophermayfield <christopher.j.mayfield@gmail.com>
2022-06-19 23:29:11 -0700

    add heapsort class

--- heapsort.js
+++ heapsort.js
@@ -1,1 +1,21 @@
 //heap sort
+
+
+//max heap class
+class MaxHeap {
+    constructor(){
+        this.heap = [];
+    }
+
+    parentIndex(index){
+        return Math.floor((index-1)/2);
+    }
+
+    leftChildIndex(index){
+        return (2*index + 1);
+    }
+
+    rightChildIndex(index){
+        return (2*index + 2);
+    }
+}
