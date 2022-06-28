288b3facd1d8adb06c6d38d6b90a1fa11f0d2d7e
christophermayfield <christopher.j.mayfield@gmail.com>
2022-06-27 18:26:40 -0700

    add heapshort function

--- heapsort.js
+++ heapsort.js
@@ -61,3 +61,18 @@ class MaxHeap {
 
 
 //max heap function below:
+
+
+function heapSort(arr){
+    var sorted = [];
+    var heap1 = new MaxHeap();
+    
+    for(let i=0; i<arr.length; i++){
+        heap1.insert(arr[i]);
+    }
+    
+    for(let i=0; i<arr.length; i++){
+        sorted.push(heap1.delete());
+    }
+    return sorted;
+}
